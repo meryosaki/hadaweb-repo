eb68537ca45a5d3e670fe16db57abb4ba002b900
oskis5 <oskis5@hotmail.com>
2016-05-21 21:19:56 +0200

    Cambio editar perfil
    Modificacion 1 editar perfil

--- HadaWeb/WebApplication1/misdatoseditar.aspx.cs
+++ HadaWeb/WebApplication1/misdatoseditar.aspx.cs
@@ -13,6 +13,13 @@ namespace WebApplication1
         {
             if (Session["USER"] == null)
                 Response.Redirect("~/inicio.aspx");
+
+            UsuarioEN us = new UsuarioEN();
+            us.Nick = Session["USER"].ToString();
+            us.mostrar_usuario();
+            Nick.Text = us.Nick;
+            Nombre.Text = us.Nombre;
+            Email.Text = us.Email;
         }
 
         protected void Confirmar(object sender, EventArgs e)
@@ -21,8 +28,8 @@ namespace WebApplication1
             cliente.Nick = Session["USER"].ToString();
             cliente.recuperarId_EN();
             cliente.Nick = Nick.Text;
-            cliente.Nombre = Text2.Text;
-            cliente.Email = Text3.Text;
+            cliente.Nombre = Nombre.Text;
+            cliente.Email = Email.Text;
             cliente.modificar_usuario();
 
         }
