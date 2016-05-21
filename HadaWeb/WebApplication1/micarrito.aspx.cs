98eac8c9cc373911916e5f35b34e47787621b10d
Kvoths <Kvoths@gmail.com>
2016-05-21 20:25:32 +0200

    Modificación carrito
    Carrito que muestra la imagen de los pedidos y el importe total

--- HadaWeb/WebApplication1/micarrito.aspx.cs
+++ HadaWeb/WebApplication1/micarrito.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using PracticaGrupalHADA;
 namespace WebApplication1
 {
     public partial class micarrito : System.Web.UI.Page
@@ -13,6 +13,14 @@ namespace WebApplication1
         {
             if (Session["USER"] == null)
                 Response.Redirect("~/inicio.aspx");
+            double precioTotal = 0;
+            string precioActual = "";
+            for (int i = 0; i < GridView1.Rows.Count; i++)
+            {
+                precioActual = GridView1.Rows[i].Cells[2].Text;
+                precioTotal += Convert.ToDouble(precioActual);
+            }
+            Importe.Text = precioTotal.ToString();
         }
 
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
